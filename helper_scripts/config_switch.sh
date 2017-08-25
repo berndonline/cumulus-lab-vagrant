8cc387666c04fe12d68b9ac506158a93416a83e2
Bernd Lehmann <bernd@lehmann.be>
2017-08-25 20:37:30 +0200

    Initial commit

---
+++ helper_scripts/config_switch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "#################################"
+echo "  Running Switch Post Config (config_switch.sh)"
+echo "#################################"
+sudo su
+
+
+## Convenience code. This is normally done in ZTP.
+
+
+
+echo "#################################"
+echo "   Finished"
+echo "#################################"
